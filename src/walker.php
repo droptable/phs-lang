0df15b3db1c24f1394725752793768f14febaef5
droptable <murdoc@raidrush.org>
2014-03-05 05:33:27 +0100

    typo

--- src/walker.php
+++ src/walker.php
@@ -205,8 +205,8 @@ abstract class Walker
   protected function enter_iface_decl($n) {}
   protected function leave_iface_decl($n) {}
   
-  protected function enter_fn_expr($n) {}
-  protected function leave_fn_expr($n) {}
+  protected function enter_fn_decl($n) {}
+  protected function leave_fn_decl($n) {}
   
   protected function enter_block($n) {}
   protected function leave_block($n) {}
